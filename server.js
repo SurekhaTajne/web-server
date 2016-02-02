9b1241706e31f64f848b2ae54ac861bb030e2717
Surekha Tajne <surekha.tajne@sigma-systems.com>
2016-02-02 14:50:17 +0530

    Initialize repo

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+var middleware = require('./middleware-file.js');
+var PORT = 3000;
+
+
+
+
+app.use(middleware.logger);
+//app.use(middleware.requireAuthentication);
+/* app.get('/', function (req, res) {
+	res.send('Hello Express !');
+});
+ */
+
+
+app.get('/about', middleware.requireAuthentication, function (req, res) {
+	res.send('About Us Page');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+
+app.listen(PORT, function () {
+	console.log('Server Started on port : ' + PORT);
+});
